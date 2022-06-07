9029e2e61dbb34efebaa875ca99d3853cdbd4696
Shaminaj Towfika Disha <stowfika88@gmail.com>
2022-06-07 23:45:53 +0600

    comments added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,37 +4,33 @@ import Products from './Products';
 import './Home.css';
 import Loading from './Loading';
 
+// Context API
 export const ProductContext = createContext('');
 
 const Home = () => {
     const [products, setProducts] = useState(null);
+
+    // fetch API using useEffect
     useEffect(() => {
         fetch('https://moveon-api-server.sbox.ali2bd.net/api/v1/customer/dummy-product')
             .then(response => response.json())
             .then(data => setProducts(data));
     }, []);
-    // console.log(products);
-    // const productsArray = [products];
-    // console.log(productsArray);
+
     if (!products) {
         return <Loading />
     }
+
     return (
         <ProductContext.Provider value={products}>
             <div className='container row row-cols-1 row-cols-md-2 mx-auto gy-4'>
                 <div className='bg-success'>
                     <h2 className='text-white text-center my-2'>Image Gallery</h2>
-                    {/* {productsArray && (productsArray || [])?.map((product, i) => <Gallery key={i} product={product} />)} */}
-                    {/* <div className="clearfix"></div> */}
                     <Gallery></Gallery>
                 </div>
                 <div>
-                    {/* {productsArray && (productsArray || [])?.map((product, i) => <Products key={i} product={product} />)} */}
                     <Products></Products>
                 </div>
-                {/* <div>
-                {<Products product={products}></Products>}
-            </div> */}
             </div>
         </ProductContext.Provider>
     );
--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -2,72 +2,51 @@ import React, { useContext, useState } from 'react';
 import { ProductContext } from './Home';
 
 const Products = () => {
+    // using Context API
     const product = useContext(ProductContext);
-    // console.log(product);
-    // const [productColor, setProductColor] = useState();
+
+    // declaring states
     const [colorId, setColorId] = useState(null);
     const [colorTitle, setColorTitle] = useState(null);
     const [sizeId, setSizeId] = useState(null);
     const [sizeTitle, setSizeTitle] = useState(null);
-    // const [isActive, setIsActive] = useState(false);
+
     const discountedProductPrice = (((product?.price?.old - product?.price?.discounted) / product?.price?.old) * 100);
-    // const productArray = [product];
-    // console.log(productArray);
-    // console.log(productArray[0]?.variation?.props[0]);
-    // const handleSelect = event => {
-    //     event.preventDefault();
-    //     const color = event.target.color.value;
-    //     setProductColor(color);
-    // }
-    // console.log(productColor);
     const color = product?.variation?.props[0].values;
     const size = product?.variation?.props[1].values;
-    // const findShoeColor = ((productArray && productColor) && (productArray || [])?.map(product => ((product?.variation?.props || []).map(property => ((property?.values || []).filter(value=> (value?.title === {productColor})))))));
-    // console.log(findShoeColor[0]);
 
+    // setting image based on color
     const selectColor = (id, title) => {
         setColorId(id);
         setColorTitle(title);
-        // setIsActive(current => !current);
     };
+
+    // setting sizes
     const selectSize = (id, title) => {
         setSizeId(id);
         setSizeTitle(title);
-        // setIsActive(current => !current);
     };
-    // const handleSelect = () => {
-    //     setIsActive(current => !current);
-    // };
-    // console.log(shoeColorId,shoeSizeId);
+
+    // finding the sku for selected color and size
     const shoeSkus = (product?.variation?.skus).find(prop => (prop.props[0] === colorId) && (prop.props[1] === sizeId));
-    // console.log(shoeSkus);
+
+    // finding object from selected shoe color and size
+    const shoeColorTitle = (product?.variation?.props[0].values).find(value => value.title === colorTitle);
+    const shoeSizeTitle = (product?.variation?.props[1].values).find(value => value.title === sizeTitle);
+
+    // finding object for shoe pic based on sku
     const shoePic = (product?.variation?.props[0].values).find(value => value.id === shoeSkus?.props[0]);
     console.log(shoePic);
 
-    const shoeColorTitle = (product?.variation?.props[0].values).find(value => value.title === colorTitle);
-    const shoeSizeTitle = (product?.variation?.props[1].values).find(value => value.title === sizeTitle);
-    // console.log('Shoe Color Title', shoeColorTitle);
-
+    // calculation of the discount from the old and discounted value
     const discountedPrice = (((shoeSkus?.price?.old - shoeSkus?.price?.discounted) / shoeSkus?.price?.old) * 100);
 
     return (
         <div>
+            {/* shoe title */}
             <div className='bg-warning'><h3 className='p-3'>{(product?.title).substr(0, 23)}</h3></div>
-            {/* <div className='bg-light p-3 my-4'>
-                <h3>Price: <span className='fw-bold'>RS. {product?.price?.discounted}</span> <span className='text-decoration-line-through text-muted'>{product?.price?.old}</span> <span className='text-danger'>({discountedProductPrice.toFixed(2)}% OFF)</span></h3>
-            </div> */}
-            {/* <form onSubmit={handleSelect} className='grid grid-cols-1 gap-3 justify-items-center mt-2'>
-                <select name="color" className="form-select">
-                    {
-                        productArray && ((productArray || [])?.map(product => ((product?.variation?.props || []).map(property => ((property?.values || []).map((value, index) => <option key={index} value={value.title}>{value.title}</option>))))))
-                    }
-                </select>
-                <input type="submit" value="Select" className="btn btn-success" />
-            </form>
-            {
-                productArray && ((productArray || [])?.map(product => ((product?.variation?.props || []).map(property => ((property?.values || []).map((value, index) => (value?.title === 'Black') && <img key={index} src={value?.image} alt={value?.title} />))))))
-            } */}
             <div>
+                {/* price section */}
                 {
                     shoeSkus ?
                         <div className='bg-light p-3 my-4'>
@@ -79,6 +58,7 @@ const Products = () => {
                         </div>
                 }
             </div>
+            {/* select color section */}
             <div className='bg-light p-3'>
                 <h4 className='bg-secondary text-white px-2 mb-3'>Select a color</h4>
                 <div>{shoeColorTitle && <h5><span className='text-secondary'>{product?.variation?.props[0]?.name}</span> : {shoeColorTitle?.title}</h5>}</div>
@@ -91,6 +71,7 @@ const Products = () => {
                     }
                 </div>
             </div>
+            {/* select size section */}
             <div className='bg-light p-3 mt-4'>
                 <h4 className='bg-secondary text-white px-2 mb-3'>Select a size</h4>
                 <div>{shoeSizeTitle && <h5><span className='text-secondary'>{product?.variation?.props[1]?.name}</span>: {shoeSizeTitle?.title}</h5>}</div>
@@ -105,6 +86,7 @@ const Products = () => {
                                 className='bg-light text-center'>{shoe.title}</p></div>)
                     }
                 </div>
+                {/* image based on sku section */}
             </div>
             {
                 (shoeSkus?.props[0] === colorId) &&
